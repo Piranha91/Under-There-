de2d62a98a7c990022a5285dd5fb432847e7e892
Justin Swanson <justin.c.swanson@gmail.com>
2021-02-28 22:26:32 -0600

    UTItem defaults

--- UnderThere/Settings/UTitem.cs
+++ UnderThere/Settings/UTitem.cs
@@ -14,8 +14,8 @@ namespace UnderThere.Settings
         public GenderTarget Gender = GenderTarget.Mutual;
         public string DispName { get; set; } = string.Empty;
         public bool IsBottom { get; set; }
-        public float Weight { get; set; } = -1;
-        public UInt32 Value { get; set; } = int.MaxValue;
+        public float Weight { get; set; } = 0.5f;
+        public UInt32 Value { get; set; } = 25;
         public List<int> Slots { get; set; } = new List<int>();
     }
 }
